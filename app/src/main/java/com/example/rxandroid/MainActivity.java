a94372c9f179c01d4f76ddbc5fd083e0517297e4
Rabbit <xe72@yandex.ru>
2019-04-22 21:51:37 +0300

    flatMap

--- app/src/main/java/com/example/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.rxandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 import android.os.Bundle;
@@ -35,19 +38,23 @@ public class MainActivity extends AppCompatActivity {
         new RepositoryImpl().getPicturesUrl("https://ru.dotabuff.com/heroes")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<PictureUrl>>() {
+                .flatMap(new Function<List<PictureUrl>, ObservableSource<PictureUrl>>() {
+                    @Override
+                    public ObservableSource<PictureUrl> apply(List<PictureUrl> pictureUrls) throws Exception {
+                        return Observable.fromArray(pictureUrls.toArray(new PictureUrl[0]));
+                    }
+                })
+                .subscribe(new Observer<PictureUrl>() {
                     @Override
                     public void onSubscribe(Disposable d) {
 
                     }
 
                     @Override
-                    public void onNext(List<PictureUrl> pictureUrls) {
-                        for (PictureUrl pictureUrl : pictureUrls) {
+                    public void onNext(PictureUrl pictureUrl) {
                             imageView = new ImageView(MainActivity.this);
                             Picasso.with(MainActivity.this).load(pictureUrl.getPicUrl()).into(imageView);
                             llMain.addView(imageView, lParams);
-                        }
                     }
 
                     @Override
