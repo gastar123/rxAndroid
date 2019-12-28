e7fb0ba17b66eb0f7db1385a8ef1eac07e2ef9a3
Rabbit <xe72@yandex.ru>
2019-12-28 13:17:53 +0300

    Изменения в rx

--- app/src/main/java/com/example/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,14 +1,5 @@
 package com.example.rxandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.schedulers.Schedulers;
-
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.ImageView;
@@ -16,7 +7,10 @@ import android.widget.LinearLayout;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,34 +32,11 @@ public class MainActivity extends AppCompatActivity {
         new RepositoryImpl().getPicturesUrl("https://ru.dotabuff.com/heroes")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .flatMap(new Function<List<PictureUrl>, ObservableSource<PictureUrl>>() {
-                    @Override
-                    public ObservableSource<PictureUrl> apply(List<PictureUrl> pictureUrls) throws Exception {
-                        return Observable.fromArray(pictureUrls.toArray(new PictureUrl[0]));
-                    }
-                })
-                .subscribe(new Observer<PictureUrl>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(PictureUrl pictureUrl) {
-                            imageView = new ImageView(MainActivity.this);
-                            Picasso.with(MainActivity.this).load(pictureUrl.getPicUrl()).into(imageView);
-                            llMain.addView(imageView, lParams);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
+                .flatMap(pictureUrls -> Observable.fromIterable(pictureUrls))
+                .subscribe(pictureUrl -> {
+                    imageView = new ImageView(MainActivity.this);
+                    Picasso.with(MainActivity.this).load(pictureUrl.getPicUrl()).into(imageView);
+                    llMain.addView(imageView, lParams);
                 });
     }
 }
